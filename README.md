4e9d2d951653760c5823f790fce18f4faa5cfdb0
francop <franco.poveda@gmail.com>
2017-01-20 19:48:51 -0300

    1.2.0

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+![stash_logo](https://cloud.githubusercontent.com/assets/11579281/22157644/439ed802-df17-11e6-9178-05591a3daa9b.png)
+==================
+
 node-cmd-stash
 ==================
 
  Usefull tool for save, list and retrive (to clipboard) your important shell commands.
+
 
 Install
 -------
@@ -17,23 +21,39 @@ Usage
 
 ```bash
 #stash some commands 
-#NOTE: stash push <command> olso works
+#NOTE: stash [push | p] <command> olso works
 
 stash docker run -d --hostname MQ37 --name mq37 -p 5672:5672 f00f2babc0bd
 stash "for i in {1..40}; do node ./publisher.js; done"
 stash sudo nmap -n -PN -sT -sU -p- remote_host
 
 # list stashed commands:
-stash list
+stash [list | l]
 ```
 
 ![List example](https://cloud.githubusercontent.com/assets/11579281/21877076/4dab4d96-d867-11e6-9044-4b487e1bc56d.png)
 
 ```bash
 # get a stashed command copied to your clipboard:
-stash get <id>
+stash [get | g] <id>
 
 # remove a stashed command from the list:
 stash pop <id>
 
+# save your list to a anonymous gits (https://gist.github.com/) and retrives the link :
+stash save 
 ```
+
+
+ Changelog
+----------
+
+1.2.0:
+
+Added "save" option. 
+
+This publish your current command stash at a anonymous gist via the github api and retrives the link to your cb.
+
+
+Added options aliases.
+
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 'use strict'
 
-const fs      = require('fs'),
-      ncp     = require('copy-paste'),
-      colors  = require('colors'),
-      sqlite3 = require('sqlite3');
+const fs = require('fs'),
+  https = require('https'),
+  ncp = require('copy-paste'),
+  colors = require('colors'),
+  sqlite3 = require('sqlite3');
 
 const homePath = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-const filePath = homePath+'/stash.db';
+const filePath = homePath + '/stash.db';
 const exists = fs.existsSync(filePath);
 const db = new sqlite3.Database(filePath);
 
@@ -20,40 +21,105 @@ db.serialize(() => {
 const stmt = db.prepare("INSERT INTO stash VALUES (?)");
 
 const options = (type) => {
-  const actions = {
-    'push': () => {
-      const argcmd = process.argv.slice(3);
-      const command = argcmd.join(' ');
-      stmt.run(command);
-      stmt.finalize();
-    },
-    'list': () => {
-      console.log("[ID]\t".green.bold + "[COMMAND]".green.bold);
-      db.each("SELECT rowid as id, cmd FROM stash", (err, row) => {
-        console.log("[" + row.id.toString().cyan.bold + "]\t" + row.cmd.toString().yellow);
-      });
-    },
-    'get': () => {
-      db.get("SELECT cmd FROM stash where rowid ==" + args[1] + " limit 1", function (err, row) {
-        if (row === undefined) {
-          console.log("id ".red + "[" + args[1].cyan.bold + "]" + " not found".red);
-          process.exit();
-
-        }
-        ncp.copy(row.cmd, () => process.exit());
-      });
-    },
-    'pop': () => {
-      db.run("DELETE FROM stash WHERE rowid = " + args[1], () => process.exit());
-    },
-    'default': () => {
-      const save = args.join(' ');
-      stmt.run(save);
-      stmt.finalize();
-    }
+  let actions = {
+    'push': push,
+    'p': push,
+    'list': list,
+    'l': list,
+    'get': get,
+    'g': get,
+    'pop': pop,
+    'save': save,
+    's': save,
+    'default': def
   };
   return (actions[type] || actions['default'])();
 }
 
+
+const push = () => {
+  const argcmd = process.argv.slice(3);
+  const command = argcmd.join(' ');
+  stmt.run(command);
+  stmt.finalize();
+}
+
+const list = () => {
+  console.log("[ID]\t".green.bold + "[COMMAND]".green.bold);
+  db.each("SELECT rowid as id, cmd FROM stash", (err, row) => {
+    console.log("[" + row.id.toString().cyan.bold + "]\t" + row.cmd.toString().yellow);
+  });
+}
+
+const pop = () => {
+  db.run("DELETE FROM stash WHERE rowid = " + args[1], () => process.exit());
+}
+
+const get = () => {
+  db.get("SELECT cmd FROM stash where rowid ==" + args[1] + " limit 1", function (err, row) {
+    if (row === undefined) {
+      console.log("id ".red + "[" + args[1].cyan.bold + "]" + " not found".red);
+      process.exit();
+
+    }
+    ncp.copy(row.cmd, () => process.exit());
+  });
+}
+const save = () => {
+  let response = "",
+    cmdList = "";
+
+  db.all("SELECT rowid as id, cmd FROM stash", (err, rows) => {
+    rows.forEach(function (value) {
+      cmdList += "[" + value.id + "]\t" + value.cmd + "\n"
+    });
+    var options = {
+      headers: {
+        "user-agent": "node-cmd-stash",
+        "Content-Type": "application/json"
+      },
+      hostname: 'api.github.com',
+      port: 443,
+      path: '/gists',
+      method: 'POST',
+    };
+    var req = https.request(options, (res) => {
+
+      if (res.statusCode !== 201) {
+        console.log("stash not uploaded...".red);
+
+
+      }
+      res.on('data', (d) => {
+        response += d.toString();
+      });
+    });
+    const data = {
+      "description": "Saved command stash",
+      "public": "true", "files": { "stash.list": { "content": cmdList } }
+    };
+    req.write(JSON.stringify(data));
+    req.end();
+
+    req.on('close', () => {
+      const res = JSON.parse(response);
+      ncp.copy(res.html_url, () => {
+        console.log("[".white.bold + res.html_url.bold.underline.magenta + "]".white.bold);
+        process.exit();
+      });
+    });
+    req.on('error', (e) => {
+      console.log("stash not uploaded...".red);
+    });
+  });
+}
+
+const def = () => {
+  const save = args.join(' ');
+  stmt.run(save);
+  stmt.finalize();
+}
+
 const args = process.argv.slice(2);
 options(args[0]);
+
